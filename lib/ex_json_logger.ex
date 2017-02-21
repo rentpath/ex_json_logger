d22792992275a619d7f1204a55c9d766a6a60759
Eric Toulson <ebtoulson@gmail.com>
2017-02-21 15:35:39 -0500

    initial commit

---
+++ lib/ex_json_logger.ex
@@ -0,0 +1,18 @@
+defmodule ExJsonLogger do
+  @moduledoc """
+  Documentation for ExJsonLogger.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExJsonLogger.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_json_logger_test.exs
@@ -0,0 +1,8 @@
+defmodule ExJsonLoggerTest do
+  use ExUnit.Case
+  doctest ExJsonLogger
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
