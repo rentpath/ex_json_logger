3ba1cd3bffaac1a6e7fa0a59050114b19a60a6d4
Eric Toulson <ebtoulson@gmail.com>
2017-05-11 10:13:08 -0400

    0.1.2 version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExJsonLogger.Mixfile do
   def project do
     [
       app: :ex_json_logger,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
