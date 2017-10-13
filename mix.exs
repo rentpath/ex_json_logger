34265fef9a6eeea8e1794334afbf9184a6bbb723
Michael Stalker <mtstalker@gmail.com>
2017-10-13 11:12:24 -0400

    Upgrade Elixir to 1.5.2
    [Card](https://www.pivotaltracker.com/story/show/521810010)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExJsonLogger.Mixfile do
     [
       app: :ex_json_logger,
       version: "0.1.2",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       package: package(),
